dbc6503ebfda81bcd26019d205053ba12e54f0cf
Bell0us <jachym2003@centrum.cz>
2021-03-29 11:12:03 +0200

    mobilní design hotov

---
+++ js/app.js
@@ -0,0 +1,212 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 120,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#696969"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 1,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 4,
+                "size_min": 0.3,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 600
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "grab"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 150,
+                "line_linked": {
+                    "opacity": 0.5261837109713353
+                }
+            },
+            "bubble": {
+                "distance": 250,
+                "size": 0,
+                "duration": 2,
+                "opacity": 0,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 95.90409590409591,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+
+/*
+ *
+ * Cards switcherino
+ *
+ */
+
+const uiCard = document.querySelector('.ui-card');
+const frontEndCard = document.querySelector('.web-card');
+const logoCard = document.querySelector('.logo-card');
+
+const uiSwitch = document.querySelector('.UI-switch')
+const frontEndSwitch = document.querySelector('.frontend-switch')
+const logoSwitch = document.querySelector('.logo-switch')
+
+
+/*
+ *
+ * Swiperino na mobilu
+ *
+ */
+
+// ! Swipe left
+
+uiCard.addEventListener('swiped-left', function (e) {
+    uiSwitch.classList.remove('switch-active')
+    frontEndSwitch.classList.add('switch-active')
+    logoSwitch.classList.remove('switch-active');
+
+    uiCard.style.zIndex = "0";
+    frontEndCard.style.zIndex = "1";
+    logoCard.style.zIndex = "0";
+});
+
+frontEndCard.addEventListener('swiped-left', function (e) {
+    uiSwitch.classList.remove('switch-active')
+    frontEndSwitch.classList.remove('switch-active')
+    logoSwitch.classList.add('switch-active');
+
+    uiCard.style.zIndex = "0";
+    frontEndCard.style.zIndex = "0";
+    logoCard.style.zIndex = "1";
+});
+
+// ! swipe right
+
+frontEndCard.addEventListener('swiped-right', function (e) {
+    uiSwitch.classList.add('switch-active');
+    frontEndSwitch.classList.remove('switch-active');
+    logoSwitch.classList.remove('switch-active');
+
+    uiCard.style.zIndex = "1";
+    frontEndCard.style.zIndex = "0";
+    logoCard.style.zIndex = "0";
+});
+
+logoCard.addEventListener('swiped-right', function (e) {
+    uiSwitch.classList.remove('switch-active')
+    frontEndSwitch.classList.add('switch-active')
+    logoSwitch.classList.remove('switch-active');
+
+    uiCard.style.zIndex = "0";
+    frontEndCard.style.zIndex = "1";
+    logoCard.style.zIndex = "0";
+});
+
+
+/*
+ *
+ * Switches
+ *
+ */
+
+uiSwitch.addEventListener('click', () => {
+    uiSwitch.classList.add('switch-active');
+    frontEndSwitch.classList.remove('switch-active');
+    logoSwitch.classList.remove('switch-active');
+
+    uiCard.style.zIndex = "1";
+    frontEndCard.style.zIndex = "0";
+    logoCard.style.zIndex = "0";
+});
+
+frontEndSwitch.addEventListener('click', () => {
+    uiSwitch.classList.remove('switch-active')
+    frontEndSwitch.classList.add('switch-active')
+    logoSwitch.classList.remove('switch-active');
+
+    uiCard.style.zIndex = "0";
+    frontEndCard.style.zIndex = "1";
+    logoCard.style.zIndex = "0";
+});
+
+logoSwitch.addEventListener('click', () => {
+    uiSwitch.classList.remove('switch-active')
+    frontEndSwitch.classList.remove('switch-active')
+    logoSwitch.classList.add('switch-active');
+
+    uiCard.style.zIndex = "0";
+    frontEndCard.style.zIndex = "0";
+    logoCard.style.zIndex = "1";
+})
